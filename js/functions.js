b41ba7030848ce5052a92a459912c0f5bdc11174
CJGarciaMusic <CJGarciaMusic@gmail.com>
2023-07-18 10:24:14 -0400

    conditional rendering of data

--- js/functions.js
+++ js/functions.js
@@ -11,82 +11,96 @@ function signup() {
     alert("bloop");
 }
 
-fetch('/assets/team_info.json')
-    .then(response => response.json())
-    .then(file => {
-        const people = document.getElementById('people');
-        file.forEach(data => {
-            // create container
-            const container = document.createElement('div');
-            container.className = "people-container";
-            people.appendChild(container);
+document.addEventListener('DOMContentLoaded', function () {
+    const currentPage = document.body.dataset.page;
 
-            // create heading
-            const heading = document.createElement('div');
-            heading.className = 'people-heading';
-            container.appendChild(heading);
+    if (currentPage === 'team') {
+        fetchTeamData();
+    } else if (currentPage === 'faq') {
+        fetchFAQData();
+    }
+});
 
-            // create image
-            const imageContainer = document.createElement('div');
-            imageContainer.className = 'profile-container';
-            heading.appendChild(imageContainer);
+function fetchTeamData() {
+    fetch('/assets/team_info.json')
+        .then(response => response.json())
+        .then(file => {
+            const people = document.getElementById('people');
+            file.forEach(data => {
+                // create container
+                const container = document.createElement('div');
+                container.className = "people-container";
+                people.appendChild(container);
 
-            const image = document.createElement('img');
-            image.src = data.picture;
-            imageContainer.appendChild(image);
+                // create heading
+                const heading = document.createElement('div');
+                heading.className = 'people-heading';
+                container.appendChild(heading);
 
-            // create text content div
-            const textcontent = document.createElement('div');
-            textcontent.classList = 'text-content';
-            heading.appendChild(textcontent);
+                // create image
+                const imageContainer = document.createElement('div');
+                imageContainer.className = 'profile-container';
+                heading.appendChild(imageContainer);
 
-            // create title
-            const title = document.createElement('div');
-            title.className = 'title';
-            title.innerText = data.name;
-            textcontent.appendChild(title);
+                const image = document.createElement('img');
+                image.src = data.picture;
+                imageContainer.appendChild(image);
 
-            // create position
-            const position = document.createElement('div');
-            position.className = 'position';
-            position.innerText = data.position;
-            textcontent.appendChild(position);
+                // create text content div
+                const textcontent = document.createElement('div');
+                textcontent.classList = 'text-content';
+                heading.appendChild(textcontent);
 
-            // create services
-            const services = document.createElement('div');
-            services.className = 'services';
-            services.innerText = data.services;
-            textcontent.appendChild(services);
+                // create title
+                const title = document.createElement('div');
+                title.className = 'title';
+                title.innerText = data.name;
+                textcontent.appendChild(title);
 
-            // create blurb
-            const blurb = document.createElement('div');
-            blurb.className = 'blurb';
-            blurb.innerText = data.blurb;
-            container.appendChild(blurb);
+                // create position
+                const position = document.createElement('div');
+                position.className = 'position';
+                position.innerText = data.position;
+                textcontent.appendChild(position);
+
+                // create services
+                const services = document.createElement('div');
+                services.className = 'services';
+                services.innerText = data.services;
+                textcontent.appendChild(services);
+
+                // create blurb
+                const blurb = document.createElement('div');
+                blurb.className = 'blurb';
+                blurb.innerText = data.blurb;
+                container.appendChild(blurb);
+            });
+        })
+        .catch(error => {
+            console.error('Error loading the team data:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error loading text content:', error);
-    });
+}
+function fetchFAQData() {
+    fetch('/assets/faq.json')
+        .then(response => response.json())
+        .then(file => {
+            const faq = document.getElementById('faq-two');
+            file.forEach(data => {
+                // create title
+                const question = document.createElement('div');
+                question.className = 'question';
+                question.innerText = data.q;
+                faq.appendChild(question);
 
-fetch('/assets/faq.json')
-    .then(response => response.json())
-    .then(file => {
-        const faq = document.getElementById('faq-two');
-        file.forEach(data => {
-            // create title
-            const question = document.createElement('div');
-            question.className = 'question';
-            question.innerText = data.q;
-            faq.appendChild(question);
-
-            // create position
-            const answer = document.createElement('p');
-            answer.className = 'answer';
-            answer.innerText = data.a;
-            faq.appendChild(answer);
+                // create position
+                const answer = document.createElement('p');
+                answer.className = 'answer';
+                answer.innerText = data.a;
+                faq.appendChild(answer);
+            });
+        })
+        .catch(error => {
+            console.error('Error loading the faq data:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error loading text content:', error);
-    });
+}
+
